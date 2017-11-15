ba9440051bedeb9cb52d5c2b2c236489b6021428
Rudi Strydom <iam@thatguy.co.za>
2017-11-15 11:04:39 +0200

    Updates

--- routes/admin.rb
+++ routes/admin.rb
@@ -4,7 +4,25 @@ module Sinatra
       module Admin
         def self.registered(app)
           admin_login = lambda do
-            erb :'admin/login'
+            erb :'admin/login', locals: { :errors => [] }
+          end
+
+          attempt_login = lambda do
+            errors = []
+            if params[:username_or_email].empty? || params[:password].empty?
+              if params[:username_or_email].empty?
+                errors << 'No username / email provided'
+                end
+              if params[:password].empty?
+                errors << 'No password provided'
+              end
+            end
+
+            erb :'admin/login', locals: { :errors => errors }
+          end
+
+          admin_register = lambda do
+            erb :'admin/register'
           end
 
           admin = lambda do
@@ -12,8 +30,10 @@ module Sinatra
             erb 'admin/index'
           end
 
-          app.get '/admin/login', &admin_login
           app.get '/admin', &admin
+          app.get '/admin/login', &admin_login
+          app.post '/admin/login', &attempt_login
+          app.get '/admin/register', &admin_register
         end
       end
     end
